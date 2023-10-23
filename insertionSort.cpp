886dc016069f4211b0093c717a97e2734ae6bd58
Vivek Kumar Tiwari <130132612+VivekTiwari-1@users.noreply.github.com>
2023-10-23 16:33:20 +0530

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -11,11 +11,6 @@ void insertionSort(int arr[], int n)
 	for (i = 1; i < n; i++) {
 		key = arr[i];
 		j = i - 1;
-
-		// Move elements of arr[0..i-1],
-		// that are greater than key, 
-		// to one position ahead of their
-		// current position
 		while (j >= 0 && arr[j] > key) {
 			arr[j + 1] = arr[j];
 			j = j - 1;
@@ -24,8 +19,6 @@ void insertionSort(int arr[], int n)
 	}
 }
 
-// A utility function to print an array
-// of size n
 void printArray(int arr[], int n)
 {
 	int i;
@@ -45,4 +38,3 @@ int main()
 
 	return 0;
 }
-// This is code is contributed by rathbhupendra
